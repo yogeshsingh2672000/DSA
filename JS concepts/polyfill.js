1412b97d0dfdfae5b3d037616cd75ed7ea033955
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2025-11-22 20:55:27 +0530

    js polyfills

---
+++ JS concepts/polyfill.js
@@ -0,0 +1,31 @@
+// map polyfill
+const arr = [1, 2, 3, 4, 5];
+
+Array.prototype.mymap = function (callback) {
+  if (callback && typeof callback !== "function")
+    throw new TypeError(`${callback} is not a function`);
+  const result = [];
+  for (let i = 0; i < this.length; i++) {
+    result.push(callback(this[i], i, this));
+  }
+  return result;
+};
+
+const newArr = arr.mymap((num) => num * 2);
+console.log(newArr); // [2,4,6,8,10]
+
+// filter polyfill
+Array.prototype.myfilter = function (cb) {
+  if (cb && typeof cb !== "function")
+    throw new TypeError(`${cb} is not a function`);
+  const result = [];
+  for (let i = 0; i < this.length; i++) {
+    if (cb(this[i], i, this)) {
+      result.push(this[i]);
+    }
+  }
+  return result;
+};
+
+const filteredArr = arr.myfilter((num) => num % 2 === 0);
+console.log(filteredArr); // [2,4]
